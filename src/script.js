119b0381e0c9144bb7a1f05f60078dfb9e89c087
HJfod <60038575+HJfod@users.noreply.github.com>
2024-12-23 13:18:44 +0200

    some sort of mobile support

--- src/script.js
+++ src/script.js
@@ -6,3 +6,10 @@ if (cover) {
         cover.classList.toggle("collapsed", this.scrollY > 10);
     });
 }
+const nav = document.querySelector("nav");
+const navBtn = document.querySelector("nav button");
+if (navBtn) {
+    navBtn.addEventListener("click", () => {
+        nav?.classList.toggle("open");
+    });
+}
