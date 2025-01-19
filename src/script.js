6d48021ac00c86eaa57dd99fa37a94a621599992
HJfod <60038575+HJfod@users.noreply.github.com>
2025-01-19 22:27:36 +0200

    perfectly smooth cover scrolling

--- src/script.js
+++ src/script.js
@@ -1,14 +1,17 @@
 // @ts-check
 
+const nav = document.querySelector("nav");
+const navBtn = document.querySelector("nav button");
 const cover = /** @type HTMLElement | null */ (document.querySelector(".cover"));
-if (cover) {
+if (cover && nav) {
     document.addEventListener("scroll", () => {
-        cover.style.setProperty("--scroll-anim-frame", `-${Math.max(Math.min(this.scrollY / (this.innerHeight / 1.5), 1), 0)}s`);
+        const style = getComputedStyle(cover);
+        const max = parseFloat(style.getPropertyValue("--max-cover-height"));
+        const min = parseFloat(style.getPropertyValue("--min-cover-height"));
+        cover.style.setProperty("--scroll-anim-frame", `-${Math.max(Math.min(this.scrollY / (max - min), 1), 0)}s`);
         // cover.classList.toggle("collapsed", this.scrollY > 50);
     });
 }
-const nav = document.querySelector("nav");
-const navBtn = document.querySelector("nav button");
 if (navBtn) {
     navBtn.addEventListener("click", () => {
         let open = nav?.classList.toggle("open");
